58d7a690dc262c2f8aa27a415b1bd244f4cf0c3b
kavidu Purnamal <kavidupurnamal2001@gmail.com>
2026-04-04 11:11:37 +0530

    Update ResourceController.java

--- src/main/java/com/sliit/smartcampus/controller/ResourceController.java
+++ src/main/java/com/sliit/smartcampus/controller/ResourceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value = "api/v1/")
+@RequestMapping(value = "Resource/")
 public class ResourceController {
 
     @Autowired
