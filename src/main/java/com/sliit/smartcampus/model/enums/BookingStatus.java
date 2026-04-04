2b812a0af9c6cadc51fcd1c19c5c052398bed9b1
kavidu Purnamal <kavidupurnamal2001@gmail.com>
2026-04-04 22:39:58 +0530

    deleted unnecessery testing files

--- src/main/java/com/sliit/smartcampus/model/enums/BookingStatus.java
+++
@@ -1,8 +0,0 @@
-package com.sliit.smartcampus.model.enums;
-
-public enum BookingStatus {
-    PENDING,     // Submitted, awaiting admin review
-    APPROVED,    // Admin approved
-    REJECTED,    // Admin rejected (reason required)
-    CANCELLED    // Cancelled after approval
-}
