dc8acbb2fca6643b52d99775ad540405a2815f88
nimeshdilshaan <nimeshdilshan126@gmail.com>
2026-04-17 23:18:50 +0530

    add Ticket Review page for technician

--- frontend/src/Components/TicketReview/TicketReview.jsx
+++ frontend/src/Components/TicketReview/TicketReview.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../NavBar/TechnicianNavBar/TechnicianNavBar";
 import "./TicketReview.css";
-import Navbar from "../NavBar/UserNavBar/UserNavbar";
 
 import {
   FiSearch, FiClock, FiCheckCircle, FiXCircle, FiSlash,
